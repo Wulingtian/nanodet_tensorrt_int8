beabc5062830e58b8b244c086439d19c335c94e2
Wulingtian <2813389262@qq.com>
2021-02-23 17:16:30 +0800

    Update nanodet_infer.cc
    更新数据预处理函数prepareImage

--- nanodet_infer.cc
+++ nanodet_infer.cc
@@ -191,43 +191,22 @@ std::vector<float> prepareImage(cv::Mat &src_img) {
     float ratio = float(INPUT_W) / float(src_img.cols) < float(INPUT_H) / float(src_img.rows) ? float(INPUT_W) / float(src_img.cols) : float(INPUT_H) / float(src_img.rows);
     cv::Mat flt_img = cv::Mat::zeros(cv::Size(INPUT_W, INPUT_H), CV_8UC3);
     cv::Mat rsz_img = cv::Mat::zeros(cv::Size(src_img.cols*ratio, src_img.rows*ratio), CV_8UC3);
-    //auto pr_start = std::chrono::high_resolution_clock::now();
-    //cv::resize(src_img, rsz_img, cv::Size(), ratio, ratio);
-    cudaResize(src_img, rsz_img);
-    //auto pr_end = std::chrono::high_resolution_clock::now();
-    //auto po_ms = std::chrono::duration<float, std::milli>(pr_end - pr_start).count();
+    cv::resize(src_img, rsz_img, cv::Size(), ratio, ratio);
     rsz_img.copyTo(flt_img(cv::Rect(0, 0, rsz_img.cols, rsz_img.rows)));
     flt_img.convertTo(flt_img, CV_32FC3);
 
-    //auto pr_end = std::chrono::high_resolution_clock::now();
-    //std::cout << "********** " << po_ms << " ms." << "********** " << std::endl;
-
-
-    //HWC TO CHW
-    //auto pr_start = std::chrono::high_resolution_clock::now();
     int channelLength = INPUT_W * INPUT_H;
     std::vector<cv::Mat> split_img = {
-            cv::Mat(INPUT_W, INPUT_H, CV_32FC1, data + channelLength * 2),
+            cv::Mat(INPUT_W, INPUT_H, CV_32FC1, data),
             cv::Mat(INPUT_W, INPUT_H, CV_32FC1, data + channelLength),
-            cv::Mat(INPUT_W, INPUT_H, CV_32FC1, data)
+            cv::Mat(INPUT_W, INPUT_H, CV_32FC1, data + channelLength * 2),
     };
-    //auto pr_end = std::chrono::high_resolution_clock::now();
-    //auto po_ms = std::chrono::duration<float, std::milli>(pr_end - pr_start).count();
-    //std::cout << "********** " << po_ms << " ms." << "********** " << std::endl;
-
-
-    auto pr_start = std::chrono::high_resolution_clock::now();
     cv::split(flt_img, split_img);
     for (int i = 0; i < 3; i++) {
             split_img[i] = (split_img[i] - img_mean[i]) / img_std[i];
     }
-    auto pr_end = std::chrono::high_resolution_clock::now();
-
-    auto po_ms = std::chrono::duration<float, std::milli>(pr_end - pr_start).count();
-    std::cout << "********** " << po_ms << " ms." << "********** " << std::endl;
     return result;
 }
-
 
 /////
 int main(int argc, const char *argv[]) {
